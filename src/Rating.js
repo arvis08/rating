c87793fac084c967da23e186e957761f354e3685
arvis08 <sainiarvindkumar3@gmail.com>
2022-09-20 20:30:02 +0530

    done

--- src/Rating.js
+++ src/Rating.js
@@ -7,19 +7,16 @@ const Rating = () => {
 
   return (
     <div>
-    {/* <p>Extremely bad</p> */}
       <label>
         <input
           type="radio"
           name="rating"
           value={1}
           onClick={() => setRating(1)}
-        //   className={1 <= (hover || rating) ? "class"+rating : ""}
         />
 
         <RiCheckboxBlankFill
           size={40}
-          
           className={1 <= (hover || rating) ? "class"+hover : ""}
           onMouseEnter={() => {
             setHover(1);
@@ -34,7 +31,6 @@ const Rating = () => {
           name="rating"
           value={2}
           onClick={() => setRating(2)}
-        //   className={2 <= (hover || rating) ? "class"+rating : ""}
         />
 
         <RiCheckboxBlankFill
@@ -54,7 +50,6 @@ const Rating = () => {
           name="rating"
           value={3}
           onClick={() => setRating(3)}
-        //   className={3 <= (hover || rating) ? "class"+rating : ""}
         />
 
         <RiCheckboxBlankFill
@@ -74,7 +69,6 @@ const Rating = () => {
           name="rating"
           value={4}
           onClick={() => setRating(4)}
-        //   className={4 <= (hover || rating) ? "class"+rating : ""}
         />
 
         <RiCheckboxBlankFill
@@ -94,7 +88,6 @@ const Rating = () => {
           name="rating"
           value={5}
           onClick={() => setRating(5)}
-        //   className={5 <= (hover || rating) ? "class"+rating : ""}
         />
 
         <RiCheckboxBlankFill
